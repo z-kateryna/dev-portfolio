5887b2a8a05727c68aff368b3c26c2072d41f6ef
Your Name <you@example.com>
2025-05-06 21:36:57 -0700

    created overall folder structure

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import "./Homepage.scss";
+import Navigation from "../../components/Navigation/Navigation"
+
+
+export default function Homepage() {
+    return (
+      <>
+        <Navigation />
+      </>
+    );
+  }
