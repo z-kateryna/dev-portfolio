2365ea9f4613430b7a63d0c945e127edb52f1a0c
Your Name <you@example.com>
2025-05-07 14:11:13 -0700

    finished responsive nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,20 @@
 import "./Navigation.scss";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import Logo from "../../assets/icons/logo.svg";
 import Arrow from "../../assets/icons/arrow-up.svg";
 import Git from "../../assets/icons/github.svg";
 import LinkdeIn from "../../assets/icons/linkedIn.svg";
 import Email from "../../assets/icons/email.svg";
+import HamburgerMenu from "../../assets/icons/hamburger.svg";
+import ArrowLight from "../../assets/icons/arrow-up-light.svg";
 
 export default function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
   return (
     <nav className="nav">
       <div className="nav__wrapper">
@@ -46,6 +54,78 @@ export default function Navigation() {
           </li>
         </ul>
         <ul className="nav__icons-container">
+          <li>
+            <a
+              className="nav__icon--link"
+              href="https://github.com/z-kateryna"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="nav__icon" src={Git} alt="github icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="nav__icon--link"
+              href="https://www.linkedin.com/in/kzabrodska"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="nav__icon" src={LinkdeIn} alt="LinkdeIn icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="nav__icon--link"
+              href="mailto:zabrodskakateryna@gmail.com"
+            >
+              <img className="nav__icon" src={Email} alt="Email icon" />
+            </a>
+          </li>
+        </ul>
+        <img
+          className="nav__hamburger--icon"
+          src={HamburgerMenu}
+          onClick={toggleMenu}
+          alt="hamburger menu icon"
+        />
+      </div>
+      <div
+        className={`nav__hamburger--container ${menuOpen ? "open" : "closed"}`}
+      >
+        <ul className="nav__hamburger--links">
+          <li className="nav__hamburger--item">
+            <NavLink to="#" className="nav__hamburger__link">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav__hamburger--item">
+            <NavLink to="#" className="nav__hamburger__link">
+              About
+            </NavLink>
+          </li>
+          <li className="nav__hamburger--item">
+            <NavLink to="#" className="nav__hamburger__link">
+              UX/UI Design
+              <img
+                src={ArrowLight}
+                alt="arrow up icon"
+                className="nav__link--icon"
+              />
+            </NavLink>
+          </li>
+          <li className="nav__hamburger--item">
+            <NavLink to="#" className="nav__hamburger__link">
+              Resume
+              <img
+                src={ArrowLight}
+                alt="arrow up icon"
+                className="nav__link--icon"
+              />
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="nav__hamburger__icons-container">
           <li>
             <a
               className="nav__icon--link"
